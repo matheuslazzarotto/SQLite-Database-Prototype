f795cc5349739f25cd2b9a202f7b7a4836748a0d
matheus lazzarotto <mvl@cesar.school>
2022-04-29 13:05:14 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,9 @@
 import sqlite3
 
-#query the database and return all records
+
 def show_all():
-    #connect to db
     conn = sqlite3.connect('landslide_database.db')
-    #create a cursor
     c = conn.cursor()
-    #query the db
     c.execute ("SELECT rowid, * FROM variables")
     items = c.fetchall()
     for item in items:
@@ -14,17 +11,16 @@ def show_all():
     conn.commit()
     conn.close()
 
-#add new record to the db
 def add_one(slope_name,slope_lat,slope_long,declivity,houses_per_square_meter,
 trees_per_square_meter,liquid_proximity,soil_umidity):
     conn = sqlite3.connect('landslide_database.db')
     c = conn.cursor()
-    c.execute("INSERT INTO variables VALUES (?,?,?,?,?,?,?,?)", (slope_name,slope_lat,slope_long,declivity,houses_per_square_meter,
+    c.execute("INSERT INTO variables VALUES (?,?,?,?,?,?,?,?)", (slope_name,slope_lat,
+    slope_long,declivity,houses_per_square_meter,
     trees_per_square_meter,liquid_proximity,soil_umidity))
     conn.commit()
     conn.close()
 
-#deleting a record of the db
 def delete_one(id):
     conn = sqlite3.connect('landslide_database.db')
     c = conn.cursor()
@@ -32,7 +28,6 @@ def delete_one(id):
     conn.commit()
     conn.close()
 
-#add more than one record to the list
 def add_many(list):
     conn = sqlite3.connect('landslide_database.db')
     c = conn.cursor()
